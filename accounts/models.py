1782ee12744f0463f17e92e3d3f7b0a545628e36
rerueter <rerueter@gmail.com>
2020-02-19 14:32:45 -0800

    puts user avatars into profile and adds interests field.

--- accounts/models.py
+++ accounts/models.py
@@ -4,3 +4,4 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile')
   picture_url = models.TextField()
+  interests = models.TextField()
