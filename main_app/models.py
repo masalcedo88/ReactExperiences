e067854af4863947914254c0d46f4f0d24360f37
rerueter <rerueter@gmail.com>
2020-02-19 10:35:45 -0800

    post merge commit

--- main_app/models.py
+++ main_app/models.py
@@ -16,6 +16,7 @@ class Adventure(models.Model):
 
 # identical related names mean that you can select adventure.bookings to return all booked adventures or user.bookings to return all bookings made by a particular user
 # autopopulate arguments for date_booked aren't working, need to consult Dalton. 
+
 class Booking(models.Model):
   adventure = models.ForeignKey(Adventure, on_delete=models.CASCADE, related_name="bookings")
   customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bookings")
