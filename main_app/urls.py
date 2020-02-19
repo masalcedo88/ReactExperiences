58108605a42e80b765cc71563a6e82b21704e945
Marco Salcedo <marcosalcedo@Marcos-MacBook-Pro.local>
2020-02-19 13:55:54 -0800

    creates new adventure view, form, url fixes nav issue with profile

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.index, name='index'),
   path('profile', views.profile, name='profile'),
+  path('adventures/new', views.adventure_create, name='adventure_create'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import AdventureForm
 
 # Create your views here.
 
@@ -6,5 +8,18 @@ from django.shortcuts import render
 def index(request):
   return render(request, 'index.html')
 
+@login_required
 def profile(request):
   return render(request, 'profile.html')
+
+@login_required
+def adventure_create(request):
+    if request.method == 'POST':
+        form = AdventureForm(request.POST)
+        if form.is_valid():
+            adventure = form.save()
+            return redirect('profile', pk=adventure.pk)
+    else:
+        form = AdventureForm()
+    context = {'form': form, 'header': "Add New Adventure"}
+    return render(request, 'adventure_form.html', context)
