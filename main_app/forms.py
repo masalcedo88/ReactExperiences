1ba3a0f8c49e52b5acdf2638444e94548899da60
elaitala <elaitala@gmail.com>
2020-02-20 09:08:25 -0800

    working directory

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,33 @@
 from django import forms
 from .models import Adventure, Booking
 
+# class DateInput(forms.DateInput):
+#     input_type = 'date'
+
 class AdventureForm(forms.ModelForm):
     class Meta:
+        # my_date_field = forms.DateField()
         model = Adventure
-        fields = ('creator', 'title', 'description', 'price', 'picture', 'location', 'date')
+        fields = [
+            'creator',
+            'title',
+            'description',
+            'price',
+            'picture',
+            'location',
+            'date'
+        ]
+
+class AdventureFormNew(forms.Form):
+    creator = forms.CharField()
+    title = forms.CharField(
+        label='',
+        widget=forms.TextInput(
+            attrs={"placeholder":"Title"}
+        )
+    )
+    description = forms.CharField()
+    price = forms.DecimalField(label='')
+    picture = forms.CharField()
+    location = forms.CharField(label='')
+    date = forms.DateField()
