b2ac7bb0ae4e7218d40b34acf781601b80498989
elaitala <elaitala@gmail.com>
2020-02-21 11:15:54 -0800

    Setting up profile picture

--- accounts/models.py
+++ accounts/models.py
@@ -3,5 +3,6 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile')
-  picture_url = models.TextField()
+  # picture_url = models.TextField()
+  picture_url = models.ImageField(upload_to='user_image', default='default.jpg')
   interests = models.TextField(null=True)
