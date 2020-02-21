a4fad7348a273892f143057577a38a16bc247cb8
Marco Salcedo <marcosalcedo@Marcos-MacBook-Pro.local>
2020-02-21 13:23:42 -0800

    adds change password functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
   path('login/', views.login, name='login'),
   path('logout/', views.logout, name='logout'),
   path('edit_info/', views.edit_info, name='edit_info'),
+  path('change_password', views.change_password, name='change_password'),
 ]
