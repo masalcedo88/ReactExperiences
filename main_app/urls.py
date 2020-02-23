3a9f3d5c9e58a35dbcd706124f19c1f7ea7fe7a8
Marco Salcedo <marcosalcedo@Marcos-MacBook-Pro.local>
2020-02-23 11:45:46 -0800

    adds about view

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,6 +8,7 @@ from . import views
 urlpatterns = [
   path('', views.index, name='index'),
   path('profile', views.profile, name='profile'),
+  path('about', views.about, name='about'),
   path('new_adventure', views.adventure_create, name='adventure_create'),
   path('my_adventures', views.adventures_list, name='adventures_list'),
   path('adventures_offered', views.adventures_offered, name='adventures_offered'),
