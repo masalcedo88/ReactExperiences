5261b72c13698bc37d2930abecb9c1c547ed89e0
rerueter <rerueter@gmail.com>
2020-02-23 20:12:30 -0800

    yo, F*** a jinja.

--- main_app/static/js/main.js
+++ main_app/static/js/main.js
@@ -1,3 +1,27 @@
 console.log("sanity check");
-$(".ui.accordion").accordion();
-$(".ui.dropdown").dropdown();
+
+const modalInject = val => {
+  $("body").append(`
+    <div class="ui basic modal deleteModal">
+    <div class="ui icon header">
+      <i class="exclamation triangle icon"></i>Delete this adventure?
+    </div>
+    <div class="modal_btn_group">
+      <div class="ui teal dismiss button modal_btn"><i class="close icon"></i>Cancel</div>
+      <a class="ui delete button modal_btn" href="/delete_post/${val}"><i class="check icon"></i>Delete</a>
+    </div>
+  </div>
+  `);
+  $(".deleteModal").modal("show");
+};
+
+$("body").on("click", ".delete", event => {
+  const postId = parseInt(event.target.id);
+  console.log(typeof postId);
+  modalInject(postId);
+});
+
+$("body").on("click", ".dismiss", () => {
+  $(".deleteModal").modal("hide");
+  $(".deleteModal").remove();
+});
